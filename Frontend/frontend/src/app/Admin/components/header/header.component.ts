1e041355e190d119dfa4dcf34e74f02bcfa51f1e
AbhijitRaut04 <abhijitmraut8010@gmail.com>
2024-08-03 13:28:05 +0530

    backend updated

--- Frontend/frontend/src/app/Admin/components/header/header.component.ts
+++ Frontend/frontend/src/app/Admin/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatButtonModule, MatInputModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
