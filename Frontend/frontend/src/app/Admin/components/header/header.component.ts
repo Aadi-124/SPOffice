8ca5d7b389e5ca3d421df423c03b263850c45ca1
momincses <mominwebdeveloper@gmail.com>
2024-08-03 17:43:14 +0530

    added footer and header

--- Frontend/frontend/src/app/Admin/components/header/header.component.ts
+++ Frontend/frontend/src/app/Admin/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [HeaderNavbarComponent, CommonModule, NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
