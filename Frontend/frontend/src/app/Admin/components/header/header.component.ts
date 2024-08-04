14c148126999d2ff6feb30edf37d0365692a267e
AbhijitRaut04 <abhijitmraut8010@gmail.com>
2024-08-04 12:01:30 +0530

    Header modified

--- Frontend/frontend/src/app/Admin/components/header/header.component.ts
+++ Frontend/frontend/src/app/Admin/components/header/header.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
-import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon'
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderNavbarComponent, CommonModule, NgFor],
-  imports: [],
+  imports: [CommonModule, NgFor, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  showDialog: boolean = true;
+
+  navElements = [
+    { img: 'https://cdn-icons-png.flaticon.com/128/1946/1946488.png', title: 'Home', active: true },
+    { img: 'https://cdn-icons-png.flaticon.com/128/3503/3503827.png', title: 'About', active: false },
+    { img: 'https://cdn-icons-png.flaticon.com/128/833/833593.png', title: 'Calendar', active: false },
+    { img: 'https://cdn-icons-png.flaticon.com/128/1077/1077063.png', title: 'Profile', active: false },
+    { img: 'https://cdn-icons-png.flaticon.com/128/1286/1286853.png', title: 'Log out', active: false }
+  ];
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth > 500) {
+      this.showDialog = true;
+    }
+    else{
+      this.showDialog = false;
+    }
+  }
+
+  setActive(index: number) {
+    this.navElements.forEach((element, i) => element.active = i === index);
+  }
 
 }
