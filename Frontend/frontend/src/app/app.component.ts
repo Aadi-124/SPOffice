1260574344636dcd45ef4e395c9aabd909d24881
momincses <mominwebdeveloper@gmail.com>
2024-08-09 18:26:21 +0530

    Added login page and sign up pages for Admin  subAdmin, police

--- Frontend/frontend/src/app/app.component.ts
+++ Frontend/frontend/src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Admin/components/header/header.component';
 import { CommonModule, NgFor } from '@angular/common';
 import { FooterComponent } from './Admin/components/footer/footer.component';
-import { SignupComponent } from './pages/signup/signup.component';
+import { PoliceSignupComponent } from './pages/police-signup/police-signup.component';
+import { SubAdminSignupComponent } from './pages/sub-admin-signup/sub-admin-signup.component';
+import { AdminSignupComponent } from './pages/admin-signup/admin-signup.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, FooterComponent, SignupComponent,NgFor],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent,NgFor, PoliceSignupComponent, SubAdminSignupComponent, AdminSignupComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
